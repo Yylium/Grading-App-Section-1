fbea42bd9207055d70d1794d5719dfce5294ddd0
Seth Brock <144707380+sethbr11@users.noreply.github.com>
2024-03-28 15:21:11 -0600

    Revert "Db"

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -1,16 +1,9 @@
-﻿using Grading_App_Section_1.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Grading_App_Section_1.Controllers
 {
     public class StudentController : Controller
     {
-        private IGradingAppRepository _repo;
-
-        public StudentController(IGradingAppRepository temp)
-        {
-            _repo = temp;
-        }
         public IActionResult Index()
         {
             return View();
--- Controllers/TAController.cs
+++ Controllers/TAController.cs
@@ -1,16 +1,9 @@
-﻿using Grading_App_Section_1.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Grading_App_Section_1.Controllers
 {
     public class TAController : Controller
     {
-        private IGradingAppRepository _repo;
-
-        public TAController(IGradingAppRepository temp)
-        {
-            _repo = temp;
-        }
         public IActionResult Index()
         {
             return View();
