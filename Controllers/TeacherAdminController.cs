f7956ea15081f149702fa6864a526a8085a29a70
Chrysanthe Belgique Johnston <belgiquc@byu.edu>
2024-04-01 11:06:20 -0600

    added comments

--- Controllers/TeacherAdminController.cs
+++ Controllers/TeacherAdminController.cs
@@ -9,5 +9,54 @@ namespace Grading_App_Section_1.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult ta_index()
+        {
+            return View()
+        }
+
+        [HttpPost] 
+        public IActionResult add_ta()
+        {
+            return View(ta_index);
+        }
+
+        [HttpGet]
+        public IActionResult Edit_ta()
+        {
+/*           var taToEdit = _repo.TAs
+            .Single(x => x.ta_netid == id);
+
+            ViewBag.Categories = _repo.Categories
+                .OrderBy(x => x.CategoryName)
+                .ToList();*/
+            return View("ta_form", taToEdit);
+
+        }
+        [HttpPost]
+        public IActionResult Edit_ta(TA updatedTa)
+        {
+/*            _repo.UpdateTask(updatedTa);
+*/
+            return RedirectToAction("ta_index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete_ta(int id)
+        {
+/*            var taToDelete = _repo.TAs
+                .Single(x => x.ta_netid == id);*/
+
+            return View("ta_confirmation", taToDelete);
+        }
+
+        [HttpPost]
+        public IActionResult Delete_ta(TA ta_netid)
+        {
+/*            _repo.RemoveTask(task);
+*/
+            return RedirectToAction("ta_index");
+        }
+
     }
 }
