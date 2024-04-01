d3b9eaf22cb1da10f2874c499451655be68c2517
Seth Brock <144707380+sethbr11@users.noreply.github.com>
2024-04-01 09:36:22 -0600

    Updated appsettings

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 
-builder.Services.AddScoped<IGradingAppRepository, EFGradingAppRepository>();
+//builder.Services.AddScoped<IGradingAppRepository, EFGradingAppRepository>();
 
 builder.Services.AddHeroicons(builder.Configuration);
 
