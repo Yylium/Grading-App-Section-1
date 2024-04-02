84727565989287f546c760f5f8474d04b8f915e9
sadieeyre <107454363+sadieeyre@users.noreply.github.com>
2024-04-02 13:27:23 -0600

    fixing view model and forms

--- Models/ViewModels/AddJudgeViewModel.cs
+++ Models/ViewModels/AddJudgeViewModel.cs
@@ -1,7 +1,20 @@
-﻿namespace Grading_App_Section_1.Models.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Grading_App_Section_1.Models.ViewModels
 {
     public class AddJudgeViewModel
     {
-        //make judge password 
+        [Required(ErrorMessage = "First Name is required")]
+        public string first_name { get; set; }
+        [Required(ErrorMessage = "Last Name is required")]
+        public string last_name { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        public string password_hash { get; set; }
+        // If a judge can be assigned to multiple rooms
+        // public List<string> schedule_room { get; set; }
+
+        // If a judge can be assigned to a single room
+        [Required(ErrorMessage = "Room is required")]
+        public string schedule_room { get; set; }
     }
 }
