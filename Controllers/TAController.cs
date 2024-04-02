903e16867434e1df7efb0cf1bffa5641ca0c5412
Preston Fitzgerald <prestonfitz@gmail.com>
2024-04-01 18:51:51 -0600

    A whole lotta updates

--- Controllers/TAController.cs
+++ Controllers/TAController.cs
@@ -5,11 +5,9 @@ namespace Grading_App_Section_1.Controllers
 {
     public class TAController : Controller
     {
-        public IActionResult Rubric()
-        {
-            return View();
-        }
+
         private IGradingAppRepository _repo;
+
 
         public TAController(IGradingAppRepository temp)
         {
@@ -18,6 +16,27 @@ namespace Grading_App_Section_1.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+        public IActionResult Rubric()
+        {
+            //var taNetid = "hge54";
+            //var classNumbers = _repo.TAs.Where(ta => ta.ta_netid == taNetid)
+            //                .Select(ta => ta.class_number).FirstOrDefault();
+
+            //var rubricItems = _repo.Rubric_Items
+            //    .Where(x => x.class_number == classNumbers).ToList();
+            List<Rubric_Item> rubric_Items = new List<Rubric_Item>();
+            var classNumbers = 414;
+            for (int i = 0; i < _repo.Rubric_Items.Count(); i++)
+            {
+                if (_repo.Rubric_Items[i].class_number == classNumbers)
+                {
+                    rubric_Items.Add(_repo.Rubric_Items[i]);
+                }
+            }
+            //var rubricItems = _repo.Rubric_Items;
+
+            return View(rubric_Items);
         }
     }
 }
