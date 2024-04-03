921e72d7cc0613605109b1e1bc34a1e451c83fdc
afank14 <103207126+afank14@users.noreply.github.com>
2024-04-02 22:30:26 -0600

    Controller changes and survey page. Won't work because it needs a change in the models that Creed said not to push yet

--- Controllers/JudgeController.cs
+++ Controllers/JudgeController.cs
@@ -13,26 +13,29 @@ namespace Grading_App_Section_1.Controllers
         }
         public IActionResult Index()
         {
-            return null;
+            return View();
         }
         public IActionResult Schedule()
         {
-            return null;
+            return View();
         }
 
-        public IActionResult Survey()
+        public IActionResult Survey(int groupId = 1)
         {
-            return null;
+            var students = _repo.Students
+                .Where(x => x.group_id == groupId).ToList();
+            
+            return View(students);
         }
 
         public IActionResult Dashboard()
         {
-            return null;
+            return View();
         }
         
         public IActionResult Rankings()
         {
-            return null;
+            return View();
         }
         
     }
