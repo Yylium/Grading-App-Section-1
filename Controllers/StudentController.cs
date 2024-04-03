80a4da61aa7123f1298cbd1bad24b3cb0d7bb50f
kinseenyland <kinseenyland@gmail.com>
2024-04-03 00:56:28 -0600

    new commits

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -16,19 +16,27 @@ namespace Grading_App_Section_1.Controllers
         [HttpGet]
         public IActionResult LinkSubmission()
         {
-            return View("~/Views/Home/Student/LinkSubmission.cshtml");
+            var netid = "ctg34";
+            
+            var creed = _repo.Students
+                .Where(x => x.student_netid == netid)
+                .FirstOrDefault();
+            return View("~/Views/Home/Student/LinkSubmission.cshtml", creed);
         }
 
         [HttpPost]
-        public IActionResult LinkSubmission(Student_Group s)
+        public IActionResult LinkSubmission(string submissionLink, int groupId)
         {
-            if (ModelState.IsValid) 
+            var group = new Student_Group
             {
-                _repo.Student_Groups.Add(s);
-                return View(s);
-            }
+                group_id = groupId,
+                submission_link = submissionLink
+            };
+            _repo.Student_Groups.Add(group);
+            //_repo.SaveChanges();
             
-            return View(); 
+            
+            return RedirectToAction("LinkSubmission"); 
         }
         
         // Build Controllers for Edit and Delete
@@ -47,10 +55,37 @@ namespace Grading_App_Section_1.Controllers
             if (ModelState.IsValid)
             {
                 //_repo.Student_Groups.Update(groupInfo);
-                return View();
+                return View("~/Views/Home/Student/Edit.cshtml");
             }
             
             return RedirectToAction("LinkSubmission");
+        }
+
+        public IActionResult InfoPage(string netid)
+        {
+            var studentinfo = _repo.Students.FirstOrDefault(s => s.student_netid == netid);
+            var groupId = studentinfo.group_id;
+            var groupsinfo = _repo.Student_Groups.FirstOrDefault(g => g.group_id == groupId);
+
+            var info = new Student
+            {
+                first_name = studentinfo.first_name,
+                group_id = studentinfo.group_id,
+                last_name = studentinfo.last_name,
+                Student_Group = new Student_Group
+                {
+                    group_id = groupsinfo.group_id,
+                    group_modifier = groupsinfo.group_modifier,
+                    group_number = groupsinfo.group_number,
+                    section_number = groupsinfo.section_number,
+                    submission_link = groupsinfo.submission_link
+
+                },
+                student_modifier = studentinfo.student_modifier,
+                student_netid = studentinfo.student_netid
+            };
+            
+            return View("~/Views/Home/Student/InfoPage.cshtml", info);
         }
     }
 }
